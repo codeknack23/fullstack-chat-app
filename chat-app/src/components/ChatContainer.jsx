fc206d9b02b6e1dd46abcafbd199ae560b1c5cce
ronak <ronakbhojayya@gmail.com>
2025-03-24 19:42:16 +0530

    minor change

--- chat-app/src/components/ChatContainer.jsx
+++ chat-app/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
@@ -19,6 +19,8 @@ const ChatContainer = () => {
 
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
+
+  const [previewImg, setPreviewImg] = useState(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
@@ -47,54 +49,77 @@ const ChatContainer = () => {
     );
   }
   return (
-    <div className="flex-1 flex flex-col overflow-auto">
-      <ChatHeader />
-      <div className="flex-1 bg-base-300 overflow-y-auto p-4 space-y-4">
-        {messages.map((message) => (
-          <div
-            key={message._id}
-            className={`chat  ${
-              message.senderId === authUser._id ? "chat-end" : "chat-start"
-            }`}
-            ref={messageEndRef}
-          >
-            <div className=" chat-image avatar">
-              <div className="size-10 rounded-full">
-                <img
-                  src={
-                    message.senderId === authUser._id
-                      ? authUser.profilePic || "/default.png"
-                      : selectedUser.profilePic || "/default.png"
-                  }
-                  alt="profile pic"
-                />
+    <>
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <div className="flex-1 bg-base-300 overflow-y-auto p-4 space-y-4">
+          {messages.map((message) => (
+            <div
+              key={message._id}
+              className={`chat  ${
+                message.senderId === authUser._id ? "chat-end" : "chat-start"
+              }`}
+              ref={messageEndRef}
+            >
+              <div className=" chat-image avatar">
+                <div className="size-10 rounded-full">
+                  <img
+                    src={
+                      message.senderId === authUser._id
+                        ? authUser.profilePic || "/default.png"
+                        : selectedUser.profilePic || "/default.png"
+                    }
+                    alt="profile pic"
+                  />
+                </div>
+              </div>
+              <div className="chat-footer text-base-content/50 mt-1">
+                <time className="text-xs ml-1">
+                  {formatMessageTime(message.createdAt)}
+                </time>
+              </div>
+              <div
+                className={`chat-bubble break-words ${
+                  message.senderId === authUser._id
+                    ? "bg-primary"
+                    : "bg-chat-my"
+                } p-1.5 flex flex-col`}
+              >
+                {message.image && (
+                  <>
+                    <img
+                      src={message.image}
+                      alt="Attachment"
+                      className="sm:max-w-[200px] rounded mb-2"
+                      onClick={() => {
+                        setPreviewImg(message.image);
+                        document.getElementById("my_modal_2").showModal();
+                      }}
+                    />
+                  </>
+                )}
+                {message.text && <p className="ml-0.5">{message.text}</p>}
               </div>
             </div>
-            <div className="chat-footer text-base-content/50 mt-1">
-              <time className="text-xs ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
-            </div>
-            <div
-              className={`chat-bubble break-words ${
-              message.senderId === authUser._id ? "bg-primary" : "bg-chat-my"
-            } p-1.5 flex flex-col`}
-            
-            >
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="sm:max-w-[200px] rounded mb-2"
-                />
-              )}
-              {message.text && <p className="ml-0.5">{message.text}</p>}
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <MessageInput />
       </div>
-      <MessageInput />
-    </div>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box bg-transparent">
+        <img
+                      src={previewImg}
+                      alt="Attachment"
+                      className="w-full max-h-100 rounded object-contain"
+                    />
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 };
 
